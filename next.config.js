6aa17dc798a5b5c41914e99ac40b9f615dae4580
htndev <htn.developer@gmail.com>
2023-05-17 12:04:10 +0200

    wip

--- next.config.js
+++ next.config.js
@@ -1,11 +1,32 @@
+const { StatsWriterPlugin } = require("webpack-stats-plugin");
+
 const withBundleStats = require("next-plugin-bundle-stats")({
-  outDir: "../artifacts",
   json: true,
+  html: false,
   compare: true,
   baseline: true,
 });
 
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+const nextConfig = {
+  webpack: (webpackConfig, { dev, isServer }) => {
+    if (!dev && !isServer) {
+      webpackConfig.plugins.push(
+        new StatsWriterPlugin({
+          filename: "./artifacts/bundle-stats.json",
+          stats: {
+            assets: true,
+            chunks: true,
+            modules: true,
+            builtAt: true,
+            hash: true,
+          },
+        })
+      );
+    }
+
+    return webpackConfig;
+  },
+};
 
 module.exports = withBundleStats(nextConfig);
